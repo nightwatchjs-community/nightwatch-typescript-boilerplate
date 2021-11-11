65cba2953868704d679b2af64c50f1f3efa3fb8e
Vaibhav Singh <vaibhav.s@browserstack.com>
2021-11-12 00:37:21 +0530

    Add: dropdown and iframe tests

---
+++ src/dropdown.ts
@@ -0,0 +1,18 @@
+import {NightwatchTests} from 'nightwatch';
+
+
+const home: NightwatchTests = {
+  'Dropdown test': async () => {
+    const dropdown = browser.page.Dropdown();
+    dropdown
+      .navigate()
+      .click('@select', () => {
+        dropdown.click('option[value=\'2\']');
+      })
+      .assert.value('@select', '2');
+
+    browser.end();
+  }
+};
+
+export default home;
